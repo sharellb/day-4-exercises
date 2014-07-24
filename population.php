e71decadf975a36ef24a23467b590922d28f4beb
Sharell Bryant <sharell.bryant@gmail.com>
2014-07-24 23:11:57 +0000

    Added connection to MySQL

--- population.php
+++ population.php
@@ -1,38 +1,55 @@
+
 <!DOCTYPE html>
 <html>
 <body>
 
-<?php 
-	$city = $_GET['city'];
+<?php
+	$localhost = 'localhost';
+	$username = 'root';
+	$password = '';
+	$connection = mysql_connect($localhost, $username, $password);
 
- 	echo '<h1>Welcome to population data online</h1>';
+	if (!$connection) {
+  		die('Unable to connect: ' . mysql_errno());
+	}
 
-	$cities = array(
-		'Philadelphia' => 1553165,
-		'Seattle' => 652405,
-		'Austin' => 885400,
-		'Miami' => 417650,
-		'Atlanta' => 447841,
-	);
+	if ($connection) {
+		$db_selected = mysql_select_db("day_4_exercises");
+		$result = mysql_query('SELECT city_name, population FROM population');
+	  
+  		if($result === FALSE) {
+    		die(mysql_error());
+		}	
+
+		$city = $_GET['city'];
+
+ 		echo '<h1>Welcome to population data online</h1>';
+
+
+
+
+				
+			while ($row = mysql_fetch_array($result)) {
+				
+				if ($row['city_name'] == $city) {
+					print "The population of " .  $row['city_name'] .  " is " . $row['population'] ;
+				}
+
+			}
+			
+			$result = mysql_query('SELECT city_name, population FROM population');
+
+			while ($row = mysql_fetch_array($result)) {
+				
+				echo '<li><a href= "/day-4-exercises/population.php?city=' . $row['city_name'] . '">' . $row['city_name'] . '</a></li>';
+				
+			}
+			
+			mysql_close($connection);
+		}
 	
-	if (!$city || !isset($cities[$city])) {
-		print "We don't have the population for that city. Try one of the cities below!";
-	}
-	else {
-
-		print "The population of " .  $city .  " is " . $cities[$city] ;
-	}
-
 ?>
 
-<h2>Look at the population of one of these cities!</h2>
-<ol>
-<?php 
-	foreach ($cities as $place => $population) {
-		echo '<li><a href= "/sharell/population.php?city=' . $place . '">' . $place . '</a></li>';
-	}
-?>
-</ol>
 
 </body>
 </html>
